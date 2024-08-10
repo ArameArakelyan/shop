e15ae94aadfdb4a196c2021016da213e80291255
ArameArakelyan <aramearakelyan.bit@gmail.com>
2024-08-10 15:24:18 +0400

    shop

---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header className="header">
+            <nav>
+                <ul className="menu">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/shop">Shop</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/shop.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+
+function Shop() {
+    const [list, setList] = useState([])
+    useEffect(() => {
+        fetch('https://dummyjson.com/products/categories')
+            .then(res => res.json())
+            .then(json => {
+                setList(json)
+            });
+    }, [])
+    
+    const [url,setUrl] = useState("https://dummyjson.com/products")
+        useEffect(() => {
+            fetch(url)
+                .then(res => res.json())
+                .then(json=> {
+                    setData(json.products)
+                });
+        },[url])
+        
+    const [value, setValue] = useState("")
+    const [search, setSearch] = useState("")
+
+    function onSubmit(e) {
+        e.preventDefault()
+        setSearch(value)
+        setValue("")
+    }
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/products/search?q=${search}`)
+            .then(res => res.json())
+            .then(json => {
+                return setData(json.products)
+            });
+    }, [search])
+
+    const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://dummyjson.com/products')
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             setData(json.products)
+    //         });
+    // }, [])
+    return (
+        <div>
+            <h1>Shop</h1>
+
+            <div style={{ display: "flex" }}>
+                <form onSubmit={onSubmit}>
+                    <input placeholder="Search..." type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+                </form>
+
+                <div>
+                    <button>
+                        Filter
+                    </button>
+                    <div>
+                        <ul className="filterlist">
+                            {list.map((e, i) => {
+                                return (
+                                    <li onClick={(e)=>setUrl(e.url)} key={i}>{e.name}</li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="shopdiv">
+                {data.map((e) => {
+                    return (
+                        <div key={e.id} className="card">
+                            <img src={e.images[0]} alt="" />
+                            <p>{e.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Shop
